974f39a5b8bdddd0d9f14992204f381d88fd63e9
mehulkapoor020 <144635746+mehulkapoor020@users.noreply.github.com>
2025-12-30 01:07:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredient_list:
 
     for x in ingredient_list:
         ingredients_string += x + ' '
-        st.subheader(x + 'Nutrition Info')
+        st.subheader(x + ' Nutrition Info')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + x)
         
         # st.text(smoothiefroot_response.json())
