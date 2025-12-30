ac19a608e628868eac33807d6d93b783498354b6
mehulkapoor020 <144635746+mehulkapoor020@users.noreply.github.com>
2025-12-30 00:51:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
 sf_df=st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
 cnx=st.connection("snowflake")
